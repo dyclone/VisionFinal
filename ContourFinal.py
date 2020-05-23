667ab3a261ec035d7309c847f5fac92612f25db3
Carlton Brady <ctombrady@gmail.com>
2020-05-23 15:50:32 -0400

    Added option to use shape factor.

--- ContourFinal.py
+++ ContourFinal.py
@@ -1,11 +1,15 @@
 import numpy as np
+from scipy import spatial
 import cv2
+from scipy.spatial.qhull import QhullError
 
 frameWidth = 640
 frameHeight = 480
 cap = cv2.VideoCapture(0)
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
+light_backgrounds = set([6])
+
 
 def empty(a):
     pass
@@ -16,7 +20,9 @@ cv2.createTrackbar("Threshold1", "Parameters", 150, 255,empty)
 cv2.createTrackbar("Threshold2", "Parameters", 255, 255,empty)
 cv2.createTrackbar("Area", "Parameters", 0, 30000, empty)
 '''
-def stackImages(scale,imgArray):
+
+
+def stackImages(scale, imgArray):
     rows = len(imgArray)
     cols = len(imgArray[0])
     rowsAvailable = isinstance(imgArray[0], list)
@@ -47,13 +53,53 @@ def stackImages(scale,imgArray):
         ver = hor
     return ver
 
-def getContours(img, imgContour):
-    contours, hierachy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
+def get_diameter_of_contour(contour):
+    """find the diameter of a contour by finding the 2 points on the convex hull of the
+    contour that are farthest apart"""
+    coords = np.squeeze(contour)
+    assert(np.shape(contour) != np.shape(coords))
+    possible_pts = coords[spatial.ConvexHull(coords).vertices]
+    distances = spatial.distance_matrix(possible_pts, possible_pts)
+    i, j = np.unravel_index(distances.argmax(), distances.shape)
+    return distances[i][j]
+
+
+def classify_shape(shape_factor):
+    """Classify the shape of a contour based on its shape factor.
+    shape factor = area/diameter^2"""
+    if 0.7 <= shape_factor <= 0.8:
+        return "circle"
+    elif 0.484 <= shape_factor <= 0.55:
+        return "square"
+    elif 0.2 <= shape_factor <= 0.3:
+        return "rectangle"
+    elif 0.44 <= shape_factor <= 0.483:
+        return "triangle"
+    elif 0.32 <= shape_factor <= 0.34:
+        return "oval"
+    elif 0.36 <= shape_factor <= 0.38:
+        return "diamond"
+    else:
+        return "unknown shape"
+
+
+def getContours(img, imgContour, test_num, use_sf=False):
+    contours, hierachy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     for cnt in contours:
         area = cv2.contourArea(cnt)
         areaMin = cv2.getTrackbarPos("Area", "Parameters")
         if area > areaMin:
+            if use_sf:
+                # Try to classify the shape using the shape factor
+                try:
+                    diameter = get_diameter_of_contour(cnt)
+                except QhullError or IndexError:
+                    print("Error while computing convex hull")
+                    use_sf = False
+                shape_factor = area / (diameter ** 2)
+                # print(shape_factor)
+                shape_factor_classification = classify_shape(shape_factor)
             cv2.drawContours(imgContour, cnt, -1, (0, 0, 255), 2)
             peri = cv2.arcLength(cnt, True)
             approx = cv2.approxPolyDP(cnt, 0.04 * peri, True)
@@ -73,8 +119,18 @@ def getContours(img, imgContour):
             else:
                 shape = "unknown shape"
 
-            cv2.putText(imgContour, shape, (x + int(w/2) - 10, y + int(h/2)), cv2.FONT_HERSHEY_SIMPLEX, .7,
-                        (255, 255, 255), 2)
+            if use_sf:
+                shape = shape_factor_classification
+
+            if test_num in light_backgrounds:
+                # black text
+                cv2.putText(imgContour, shape, (x + int(w/2) - 10, y + int(h/2)), cv2.FONT_HERSHEY_SIMPLEX, .7,
+                            (0, 0, 0), 2)
+            else:
+                # white labels
+                cv2.putText(imgContour, shape, (x + int(w / 2) - 10, y + int(h / 2)), cv2.FONT_HERSHEY_SIMPLEX, .7,
+                            (255, 255, 255), 2)
+
 
 def get_test_params(a_file):
     tester = open(a_file)
@@ -87,7 +143,8 @@ def get_test_params(a_file):
     tester.close()
     return inputs
 
-def shape_detection(a_file_name):
+
+def shape_detection(a_file_name, test_num, use_sf=False):
     cv2.namedWindow("Parameters")
     cv2.resizeWindow("Parameters", 640, 240)
     cv2.createTrackbar("Threshold1", "Parameters", 150, 255, empty)
@@ -115,11 +172,11 @@ def shape_detection(a_file_name):
 
         # Use imgCanny to develop contours and put the results in the original image
         imgContour = img.copy()
-        getContours(imgCanny, imgContour)
+        getContours(imgCanny, imgContour, test_num, use_sf=use_sf)
 
         # Use imgDil to develop contours and put the results in the original image
         imgContour2 = img.copy()
-        getContours(imgdil, imgContour2)
+        getContours(imgdil, imgContour2, test_num, use_sf=use_sf)
 
         # Store the results at each step of getting an image, finding the contours and identifying shapes
         imgStack = stackImages(0.8, ([img, imgCanny, imgdil],
@@ -130,11 +187,26 @@ def shape_detection(a_file_name):
         if cv2.waitKey(1) & 0xff == ord('q'):
             break
 
+
+def get_input_list_string(img_files):
+    result = ""
+    test_num = 1
+    for img_name in img_files:
+        result += str(test_num) + ": "+img_name[0] + "\n"
+        test_num += 1
+    return result
+
+
 def main():
     inputs = get_test_params("testparameters.txt")
-    test_num = int(input("Enter the test you would like to run:")) - 1
+    test_num = int(input("Enter the test number you would like to run:\n" + get_input_list_string(inputs))) - 1
     test_image = inputs[test_num][0]
-    shape_detection(test_image)
+    method = int(input("Enter 1 to use the Douglas-Pecker split-and-merge algorithm, Enter 2 to use shape factor\n"))
+    if method == 2:
+        shape_detection(test_image, test_num, use_sf=True)
+    else:
+        shape_detection(test_image, test_num)
+
 
 if __name__ == '__main__':
     main()
